3096ec432dd019d526a46d0d1376d1e428bd7dd3
abbaskarake <v8v.web@gmail.com>
2020-04-21 17:48:13 +0300

    projet spring

---
+++ src/main/java/com/abbas/SpringMVCJalon0/SpringMvcJalon0Application.java
@@ -0,0 +1,13 @@
+package com.abbas.SpringMVCJalon0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcJalon0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcJalon0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/abbas/SpringMVCJalon0/SpringMvcJalon0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abbas.SpringMVCJalon0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcJalon0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
